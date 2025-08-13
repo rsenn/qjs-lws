98a8323b34ff1b628e17f7c8622821a5a2610646
Roman Senn <roman.l.senn@gmail.com>
2025-08-14 00:50:55 +0200

    ...

--- lws-sockaddr46.c
+++ lws-sockaddr46.c
@@ -7,11 +7,6 @@
 JSClassID lwsjs_sockaddr46_class_id;
 static JSValue lwsjs_sockaddr46_proto, lwsjs_sockaddr46_ctor;
 
-enum {
-  PROP_FAMILY = 0,
-  PROP_ADDRESS,
-};
-
 static inline LWSSocket*
 lwsjs_sockaddr46_data(JSValueConst value) {
   return JS_GetOpaque(value, lwsjs_sockaddr46_class_id);
@@ -25,18 +20,36 @@ lwsjs_sockaddr46_data2(JSContext* ctx, JSValueConst value) {
 static JSValue
 lwsjs_sockaddr46_constructor(JSContext* ctx, JSValueConst new_target, int argc, JSValueConst argv[]) {
   lws_sockaddr46 sa = {0};
-  uint8_t* p;
-  size_t len;
+  int argi = 0;
 
-  if((p = get_buffer(ctx, argc, argv, &len))) {
-    if(len == 4) {
-      sa.sa4.sin_family = AF_INET;
-      memcpy(&sa.sa4.sin_addr, p, len);
-    } else if(len == 16) {
-      sa.sa6.sin6_family = AF_INET6;
-      memcpy(&sa.sa6.sin6_addr, p, len);
-    } else
-      memcpy(&sa, p, MIN(len, sizeof(sa)));
+  if(argc > argi && JS_IsNumber(argv[argi])) {
+    sa.sa4.sin_family = to_uint32(ctx, argv[argi]);
+    argi++;
+  }
+
+  if(argc > argi && JS_IsNumber(argv[argi])) {
+    sa.sa4.sin_port = htons(to_uint32(ctx, argv[argi]));
+    argi++;
+  }
+
+  if(argc > argi) {
+    uint8_t* p;
+    size_t len;
+    const char* str;
+
+    if((p = get_buffer(ctx, argc - argi, argv - argi, &len))) {
+      if(len == 4 && (sa.sa4.sin_family == 0 || sa.sa4.sin_family == AF_INET)) {
+        sa.sa4.sin_family = AF_INET;
+        memcpy(&sa.sa4.sin_addr, p, len);
+      } else if(len == 16 && (sa.sa6.sin6_family == 0 || sa.sa6.sin6_family == AF_INET6)) {
+        sa.sa6.sin6_family = AF_INET6;
+        memcpy(&sa.sa6.sin6_addr, p, len);
+      } else if(argi == 0)
+        memcpy(&sa, p, MIN(len, sizeof(sa)));
+    } else if((str = JS_ToCString(ctx, argv[0]))) {
+      lws_sa46_parse_numeric_address(str, &sa);
+      JS_FreeCString(ctx, str);
+    }
   }
 
   JSValue obj = JS_NewArrayBufferCopy(ctx, (uint8_t*)&sa, sizeof(sa));
@@ -60,6 +73,70 @@ fail:
   return JS_EXCEPTION;
 }
 
+enum {
+  METHOD_TO_STRING = 0,
+  METHOD_COMPARE,
+  METHOD_ON_NET,
+};
+
+static JSValue
+lwsjs_sockaddr46_methods(JSContext* ctx, JSValueConst this_val, int argc, JSValueConst argv[], int magic) {
+  size_t len;
+  lws_sockaddr46* sa;
+  JSValue ret = JS_UNDEFINED;
+
+  if(!(sa = (lws_sockaddr46*)JS_GetArrayBuffer(ctx, &len, this_val)))
+    return JS_EXCEPTION;
+
+  if(len < sizeof(*sa))
+    return JS_ThrowRangeError(ctx, "SockAddr64 must have length %zu", sizeof(*sa));
+
+  switch(magic) {
+    case METHOD_TO_STRING: {
+      char buf[64];
+      int r = lws_sa46_write_numeric_address(sa, buf, sizeof(buf));
+      ret = JS_NewStringLen(ctx, buf, r);
+      break;
+    }
+
+    case METHOD_COMPARE: {
+      size_t len2;
+      lws_sockaddr46* sa2;
+
+      if(!(sa2 = (lws_sockaddr46*)JS_GetArrayBuffer(ctx, &len, argv[0])))
+        return JS_EXCEPTION;
+
+      if(len2 < sizeof(*sa2))
+        return JS_ThrowRangeError(ctx, "SockAddr64 must have length %zu", sizeof(*sa));
+
+      ret = JS_NewInt32(ctx, lws_sa46_compare_ads(sa, sa2));
+      break;
+    }
+
+    case METHOD_ON_NET: {
+      size_t len2;
+      lws_sockaddr46* sa2;
+
+      if(!(sa2 = (lws_sockaddr46*)JS_GetArrayBuffer(ctx, &len, argv[0])))
+        return JS_EXCEPTION;
+
+      if(len2 < sizeof(*sa2))
+        return JS_ThrowRangeError(ctx, "SockAddr64 must have length %zu", sizeof(*sa));
+
+      ret = JS_NewBool(ctx, !lws_sa46_on_net(sa, sa2, to_uint32(ctx, argv[1])));
+      break;
+    }
+  }
+
+  return ret;
+}
+
+enum {
+  PROP_FAMILY = 0,
+  PROP_PORT,
+  PROP_ADDRESS,
+};
+
 static JSValue
 lwsjs_sockaddr46_get(JSContext* ctx, JSValueConst this_val, int magic) {
   size_t len;
@@ -70,11 +147,18 @@ lwsjs_sockaddr46_get(JSContext* ctx, JSValueConst this_val, int magic) {
     return JS_EXCEPTION;
 
   if(len < sizeof(*sa))
-    return JS_ThrowRangeError(ctx, "SockAddr64 must have legnth %zu", sizeof(*sa));
+    return JS_ThrowRangeError(ctx, "SockAddr64 must have length %zu", sizeof(*sa));
 
   switch(magic) {
     case PROP_FAMILY: {
       ret = JS_NewInt32(ctx, sa->sa4.sin_family);
+      break;
+    }
+    case PROP_PORT: {
+      switch(sa->sa4.sin_family) {
+        case AF_INET: ret = JS_NewUint32(ctx, ntohs(sa->sa4.sin_port)); break;
+        case AF_INET6: ret = JS_NewUint32(ctx, ntohs(sa->sa6.sin6_port)); break;
+      }
       break;
     }
     case PROP_ADDRESS: {
@@ -96,20 +180,80 @@ lwsjs_sockaddr46_get(JSContext* ctx, JSValueConst this_val, int magic) {
   return ret;
 }
 
-static const JSClassDef lws_sockaddr46_class = {"LWSSocket"};
+static JSValue
+lwsjs_sockaddr46_set(JSContext* ctx, JSValueConst this_val, JSValueConst value, int magic) {
+  size_t len;
+  lws_sockaddr46* sa;
+  JSValue ret = JS_UNDEFINED;
+
+  if(!(sa = (lws_sockaddr46*)JS_GetArrayBuffer(ctx, &len, this_val)))
+    return JS_EXCEPTION;
+
+  if(len < sizeof(*sa))
+    return JS_ThrowRangeError(ctx, "SockAddr64 must have length %zu", sizeof(*sa));
+
+  switch(magic) {
+    case PROP_PORT: {
+      switch(sa->sa4.sin_family) {
+        case AF_INET: sa->sa4.sin_port = htons(to_uint32(ctx, value)); break;
+        case AF_INET6: sa->sa6.sin6_port = htons(to_uint32(ctx, value)); break;
+      }
+    }
+    case PROP_ADDRESS: {
+      uint8_t* p;
+      size_t len;
+
+      if((p = JS_GetArrayBuffer(ctx, &len, value))) {
+        if(len == sizeof(sa->sa4.sin_addr)) {
+          sa->sa4.sin_family = AF_INET;
+          memcpy(&sa->sa4.sin_addr, p, len);
+        } else if(len == sizeof(sa->sa6.sin6_addr)) {
+          sa->sa6.sin6_family = AF_INET6;
+          memcpy(&sa->sa6.sin6_addr, p, len);
+        }
+      }
+
+      break;
+    }
+  }
+
+  return ret;
+}
+
+static const JSClassDef lws_sockaddr46_class = {
+    "LWSSocket",
+};
 
 static const JSCFunctionListEntry lws_sockaddr46_proto_funcs[] = {
     JS_CGETSET_MAGIC_FLAGS_DEF("family", lwsjs_sockaddr46_get, 0, PROP_FAMILY, JS_PROP_ENUMERABLE),
-    JS_CGETSET_MAGIC_FLAGS_DEF("address", lwsjs_sockaddr46_get, 0, PROP_ADDRESS, JS_PROP_ENUMERABLE),
+    JS_CGETSET_MAGIC_FLAGS_DEF("port", lwsjs_sockaddr46_get, lwsjs_sockaddr46_set, PROP_PORT, 0),
+    JS_CGETSET_MAGIC_FLAGS_DEF("address", lwsjs_sockaddr46_get, lwsjs_sockaddr46_set, PROP_ADDRESS, JS_PROP_ENUMERABLE),
+    JS_CFUNC_MAGIC_DEF("toString", 0, lwsjs_sockaddr46_methods, METHOD_TO_STRING),
+    JS_CFUNC_MAGIC_DEF("compare", 1, lwsjs_sockaddr46_methods, METHOD_COMPARE),
+    JS_CFUNC_MAGIC_DEF("onNet", 2, lwsjs_sockaddr46_methods, METHOD_ON_NET),
 
     JS_PROP_STRING_DEF("[Symbol.toStringTag]", "LWSSockAddr46", JS_PROP_CONFIGURABLE),
 };
+
+JSValue
+js_arraybuffer_prototype(JSContext* ctx) {
+  uint8_t buf[4];
+
+  JSValue obj = JS_NewArrayBufferCopy(ctx, buf, sizeof(buf));
+  JSValue proto = JS_GetPrototype(ctx, obj);
+  JS_FreeValue(ctx, obj);
+  return proto;
+}
 
 int
 lwsjs_sockaddr46_init(JSContext* ctx, JSModuleDef* m) {
   JS_NewClassID(&lwsjs_sockaddr46_class_id);
   JS_NewClass(JS_GetRuntime(ctx), lwsjs_sockaddr46_class_id, &lws_sockaddr46_class);
-  lwsjs_sockaddr46_proto = JS_NewObjectProto(ctx, JS_NULL);
+
+  JSValue proto = js_arraybuffer_prototype(ctx);
+  lwsjs_sockaddr46_proto = JS_NewObjectProto(ctx, proto);
+  JS_FreeValue(ctx, proto);
+
   JS_SetPropertyFunctionList(ctx, lwsjs_sockaddr46_proto, lws_sockaddr46_proto_funcs, countof(lws_sockaddr46_proto_funcs));
 
   lwsjs_sockaddr46_ctor = JS_NewCFunction2(ctx, lwsjs_sockaddr46_constructor, "LWSSockAddr46", 0, JS_CFUNC_constructor, 0);
